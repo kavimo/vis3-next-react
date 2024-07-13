5560e37e79ee7258366f6f5470df1be1eaae9731
Kavimo <info@kavimo.com>
2024-07-13 15:56:54 +0330

    v1.0.1

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,42 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const Vis3 = (props) => {
-  // Ref to store the container ID
-  const containerIDRef = useRef('');
-
-  if (!containerIDRef.current) {
-    // Generate a unique ID for the container if it hasn't been generated yet
-    containerIDRef.current = Math.random().toString(36).substring(2);
-  }
+  // State to store the container ID
+  const [containerID, setContainerID] = useState('')
 
   useEffect(() => {
-    const generatedID = containerIDRef.current;
+    // Generate a unique ID for the container
+    const generatedID = Math.random().toString(36).substring(2)
+    setContainerID(generatedID)
 
     // If a callback function is provided, set it on the window object
     if (typeof props.onLoad === 'function') {
-      window[`vis_callback_${generatedID}`] = props.onLoad;
+      window[`vis_callback_${generatedID}`] = props.onLoad
     }
 
     // Create a script element and set its attributes
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://${props?.domainName || ''}/${props?.ID || ''}/embed?container=${generatedID}&callback=vis_callback_${generatedID}`;
+    const script = document.createElement('script')
+    script.async = true
+    script.src = `https://${props?.domainName || ''}/${props?.ID || ''}/embed?container=${generatedID}&callback=vis_callback_${generatedID}`
 
     // Append the script to the document body
-    document.body.appendChild(script);
+    document.body.appendChild(script)
 
     // Cleanup function to remove the script element when the component unmounts or props change
     return () => {
-      document.body.removeChild(script);
+      document.body.removeChild(script)
       // Remove the callback function from the window object
       if (typeof props.onLoad === 'function') {
-        delete window[`vis_callback_${generatedID}`];
+        delete window[`vis_callback_${generatedID}`]
       }
-    };
-  }, [props]); // Run this effect whenever props change
+    }
+  }, [props]) // Run this effect whenever props change
 
   return (
     <>
-      <div id={containerIDRef.current}></div>
+      <div id={containerID}></div>
     </>
-  );
+  )
 }
 
 export { Vis3 }
